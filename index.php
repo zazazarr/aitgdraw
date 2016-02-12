8cb52d06d026d3dd6567d715f2acc777417f3eb1
jewk <jewk@lol.com>
2016-02-12 19:41:53 +0200

    rofl

--- index.php
+++ index.php
@@ -30,13 +30,13 @@ if ($_FILES["f"])
 		if (ord($hdr2[0]) != 40 || ord($hdr2[1]) != 0 || ord($hdr2[2]) != 0 || ord($hdr2[3]) != 0)
 			echo "ERROR: bad header size";
 			
-		$bmp_width  = ord($hdr[4]) + (ord($hdr[5])<<8) + (ord($hdr[6])<<16)  + (ord($hdr[7])<<24);
-		$bmp_height = ord($hdr[8]) + (ord($hdr[9])<<8) + (ord($hdr[10])<<16) + (ord($hdr[11])<<24);
+		$bmp_width  = ord($hdr2[4]) + (ord($hdr2[5])<<8) + (ord($hdr2[6])<<16)  + (ord($hdr2[7])<<24);
+		$bmp_height = ord($hdr2[8]) + (ord($hdr2[9])<<8) + (ord($hdr2[10])<<16) + (ord($hdr2[11])<<24);
 		
 		if (ord($hdr2[12]) != 1 || ord($hdr2[13] != 0)
 			echo ("ERROR: bad color planes");
 		
-		$bmp_bpp = ord($hdr[14]) + (ord($hdr[15])<<8);
+		$bmp_bpp = ord($hdr2[14]) + (ord($hdr2[15])<<8);
 		
 		if (ord($hdr2[16]) != 0 || ord($hdr2[17]) != 0 || ord($hdr2[18]) != 0 || ord($hdr2[19]) != 0)
 			echo "ERROR: compression not supported";
