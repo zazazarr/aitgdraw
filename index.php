6c2f27ab0ab0ef77628b1be660cb8f30b1829235
jewk <jewk@lol.com>
2016-02-12 19:40:23 +0200

    rofl

--- index.php
+++ index.php
@@ -21,8 +21,26 @@ if ($_FILES["f"])
 		
 		$hdr_s = ord($f_hdr[2]) + (ord($f_hdr[3]) << 8);
 		if ($hdr_s != filesize($_FILES["f"]["tmp_name"]))
-			echo "ERROR: bad bitmap";
+			echo "ERROR: bad bmp size";
 			
+		$hdr2 = fread($f, 40);
+		if ($hdr2 == FALSE || strlen($hdr2) != 40)
+			echo "ERROR: bad header";
+			
+		if (ord($hdr2[0]) != 40 || ord($hdr2[1]) != 0 || ord($hdr2[2]) != 0 || ord($hdr2[3]) != 0)
+			echo "ERROR: bad header size";
+			
+		$bmp_width  = ord($hdr[4]) + (ord($hdr[5])<<8) + (ord($hdr[6])<<16)  + (ord($hdr[7])<<24);
+		$bmp_height = ord($hdr[8]) + (ord($hdr[9])<<8) + (ord($hdr[10])<<16) + (ord($hdr[11])<<24);
+		
+		if (ord($hdr2[12]) != 1 || ord($hdr2[13] != 0)
+			echo ("ERROR: bad color planes");
+		
+		$bmp_bpp = ord($hdr[14]) + (ord($hdr[15])<<8);
+		
+		if (ord($hdr2[16]) != 0 || ord($hdr2[17]) != 0 || ord($hdr2[18]) != 0 || ord($hdr2[19]) != 0)
+			echo "ERROR: compression not supported";
+		
 		echo "all good";
 	}
 }
