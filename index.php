cd032e07f680f9777711191b7b221be0e0d3e61c
jewk <jewk@lol.com>
2016-02-12 15:53:26 +0200

    rofl

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo $_SERVER['HTTP_USER_AGENT'];
+if ($_POST["a1"])
+	echo "a";
+else
+	echo "b";
 ?>
+
+
+<form method="post">
+<input type="text" name="a1">
+<input type="text" name="a2">
+<input type="submit">
+</form>
