2133078591c827739cc8e9c9acdbf6113b83aaee
jewk <jewk@lol.com>
2016-02-12 18:59:18 +0200

    rofl

--- index.php
+++ index.php
@@ -19,14 +19,10 @@ if ($_FILES["f"])
 		if ($f_hdr[0] != 'B' || $f_hdr[1] != 'M')
 			echo "ERROR: not a bmp";
 		
-		$hdr_s = ord($f_hdr[2]) + ord($f_hdr[3])<<8;
+		$hdr_s = ($f_hdr[2] + $f_hdr[3]) << 8;
 		if ($hdr_s != filesize($_FILES["f"]["tmp_name"]))
 			echo "ERROR: bad bitmap";
-			echo $hdr_s;
-			echo "<br>";
-			echo filesize($_FILES["f"]["tmp_name"]);
-			echo "<br>";
-			echo ord($f_hdr[2])."<br>".ord($f_hdr[3]);
+			
 		echo "all good";
 	}
 }
