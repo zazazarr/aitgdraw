13a4b1313ac7980bdcb85a6f514460eee9755cb2
jewk <jewk@lol.com>
2016-02-12 21:05:31 +0200

    rofl

--- index.php
+++ index.php
@@ -64,8 +64,9 @@ if ($_FILES["f"])
 				$pos = 0;
 				for ($p = 0; $p < $bmp_width; $p++)
 				{
-					$clr = sprintf("%X", substr($row, pos, 3));
-					echo "<br>".$clr."   ---   ".substr($row,pos,3);
+					$n = ord($row[pos]) + (ord($row[pos+1]<<8) + (ord($row[pos+2]<<16);
+					$clr = sprintf("%X", n);
+					echo "<br>".$clr."   ---   ".n;
 					$pos += 3;
 				}
 				
